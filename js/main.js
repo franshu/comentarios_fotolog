a46a8fda7db1f6ecb950fb4a4e62e5f89dbc6841
franshu <fran.ojeda208@gmail.com>
2017-06-19 01:31:50 -0300

    guardar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-/*aquí va tu código*/
+function saveData(){
+	var name=document.getElementById("name").value;
+	var coment=document.getElementById("valor").value;
+	sessionStorage.setItem(name, coment);
+}
+savedata();
+
+function cleanData(){
+	document.getElementById("data").innerHTML="clearcomentarios. savecomentarios";
+}
+cleanData();
+
+function borrarTodo(){
+	sessionStorage.clear(); recoverData();
+}
+borrar();
